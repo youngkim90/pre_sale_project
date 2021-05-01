d0fb0d610c68314717eae9ff3efcd99419ee3395
0kim <ceo.youngkim@gmail.com>
2021-05-02 01:03:18 +0900

    only view page

---
+++ views/content.js
@@ -0,0 +1,22 @@
+module.exports = {
+    content1:function(data) {
+        if(data=='menu_1') {
+            var content = `<h1>반달섬 마리나 큐브</h1>
+                <h2>상담문의 신청시 즉시 답변드립니다.</h2>
+                <h2>대표번호 1877-7449</h2>
+                <div class="contents">
+                    <img src="./images/content/cont1.png"/> 
+                </div>
+                <div class="contents">
+                    <img src="./images/content/cont2.png"/> 
+                </div>
+                <div class="contents">
+                    <img src="./images/content/cont3.png"/> 
+                </div>
+                <div class="contents">
+                    <img src="./images/content/cont4.png"/> 
+                </div>`;
+            return content;
+        }
+    }
+}
