eac1d7ba3adedcd83b5b8ae1e12ef1efa7617a0e
김영완 <ceo.youngkim@gmail.com>
2021-05-11 15:31:25 +0900

    98% Complete It is refactoring and test the source.

--- router/db.js
+++ router/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host : 'localhost',
     port : '3306',
-    user : 'root',
-    password : '111111',
+    user : 'admin',
+    password : 'sejin123',
     database : 'presale'
 })
 db.connect();
