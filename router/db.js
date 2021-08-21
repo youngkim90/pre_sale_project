630a585e324c8bbfc3c829df2c9a606c285d78e4
youngkim90 <youngkim90@cafe24.com>
2021-08-21 20:44:18 +0900

    update

--- router/db.js
+++ router/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const db = mysql.createConnection({
-    host : 'artiscube.cafe24app.com',
+    host : 'marinacube.cafe24app.com',
     port : '3306',
     user : 'youngkim90',
     password : 'motor2021',
